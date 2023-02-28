0780c8e121df0958feb3303fdb9e8f138b49c0bb
Vinicius <vinicius.lima.brasil1@gmail.com>
2023-02-28 14:25:07 -0300

    button component story created at storybook

--- packages/react/src/index.tsx
+++ packages/react/src/index.tsx
@@ -1,8 +1,30 @@
 import { styled } from "./styles";
+import { ComponentProps } from "react";
 
 export const Button = styled("button", {
   fontFamily: "$default",
   backgroundColor: "$ignite500",
   borderRadius: "$md",
-  padding: "$4",
+  border: 0,
+  fontWeight: "bold",
+  color: "$white",
+
+  variants: {
+    variant: {
+      small: {
+        fontSize: 12,
+        padding: "$2 $4",
+      },
+      big: {
+        fontSize: 16,
+        padding: "$3 $6",
+      },
+    },
+  },
+
+  defaultVariants: {
+    variant: "small",
+  },
 });
+
+export type ButtonProps = ComponentProps<typeof Button>;

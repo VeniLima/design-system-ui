61c80bc53c1e7604da8232d7a2924efcd19049d3
Vinicius <vinicius.lima.brasil1@gmail.com>
2023-03-02 22:28:17 -0300

    checkbox added to storybook

--- packages/react/src/components/Checkbox/index.tsx
+++ packages/react/src/components/Checkbox/index.tsx
@@ -1,3 +1,16 @@
-export function Checkbox() {
-  return <CheckboxContainer></CheckboxContainer>;
+import { Check } from "phosphor-react";
+import { ComponentProps } from "react";
+import { CheckboxContainer, CheckboxIndicator } from "./styles";
+
+export interface CheckboxProps
+  extends ComponentProps<typeof CheckboxContainer> {}
+
+export function Checkbox(props: CheckboxProps) {
+  return (
+    <CheckboxContainer {...props}>
+      <CheckboxIndicator>
+        <Check weight="bold" />
+      </CheckboxIndicator>
+    </CheckboxContainer>
+  );
 }
